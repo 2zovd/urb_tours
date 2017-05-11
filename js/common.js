e21a659fbb9544a490cf0b6d180d39d085740946
Tuzov <dmy2zov@gmail.com>
2017-05-11 14:18:55 +0300

    hover effects finish

--- js/common.js
+++ js/common.js
@@ -2,3 +2,11 @@ $('.day-block').click(function() {
 	$('.day-block').removeClass('active-day');
 	$(this).toggleClass('active-day');
 });
+
+$('.send').click(function() {
+      $('.success').addClass('active');
+      setTimeout(function() {
+            $('.success').removeClass('active').fadeOut();
+      }, 7000);
+      return false;
+});
